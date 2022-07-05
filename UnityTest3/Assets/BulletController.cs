601cd6be6fd61008747d8922f370a89c790af252
duswn0018@naver.com <duswn0018@naver.com>
2022-07-05 17:18:31 +0900

    2022/07/05 수업예제

--- UnityTest3/Assets/BulletController.cs
+++ UnityTest3/Assets/BulletController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class BulletController : MonoBehaviour
 {
     public float speed = 10.0f;
+
+    GameObject player;
+
     // private Rigidbody bulletRd;
 
     // Start is called before the first frame update
@@ -19,17 +22,60 @@ public class BulletController : MonoBehaviour
         
     }
 
-    public void Shoot()
+    public void ShootToEnemy()
     {
         GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, speed));
     }
 
+    public void ShootToPlayer()
+    {
+        player = GameObject.Find("Player");
+        Vector3 dir = player.transform.position - this.transform.position;
+        GetComponent<Rigidbody>().AddForce(dir*speed/10);
+    }
+
     public void OnCollisionEnter(Collision coll)
     {
+        print("name : " + coll.collider.name);
+        
         if (coll.collider.tag == "ENEMY")
         {
+            // UI Ä«¿îÅÍ +1
+            GameObject manager = GameObject.Find("ScoreManager");
+            manager.GetComponent<ScoreScript>().IncScore();
+
             // ÃÑ¾Ë ÆÄ±«
             Destroy(gameObject);
+        }
+
+        if (coll.collider.name == "TARGET1")
+        {
+            // UI Ä«¿îÅÍ +1
+            GameObject manager = GameObject.Find("ScoreManager");
+            manager.GetComponent<ScoreScript>().IncScore(10);
+
+            // ÃÑ¾Ë ÆÄ±«
+            Destroy(gameObject, 0.0f);
+        }
+
+        if (coll.collider.name == "TARGET2")
+        {
+            // UI Ä«¿îÅÍ +1
+            GameObject manager = GameObject.Find("ScoreManager");
+            manager.GetComponent<ScoreScript>().IncScore(8);
+
+            // ÃÑ¾Ë ÆÄ±«
+            Destroy(gameObject, 0.0f);
+        }
+
+        if (coll.collider.name == "TARGET3")
+        {
+            // UI Ä«¿îÅÍ +1
+            GameObject manager = GameObject.Find("ScoreManager");
+            manager.GetComponent<ScoreScript>().IncScore(5);
+
+            // ÃÑ¾Ë ÆÄ±«
+            Destroy(gameObject, 0.0f);
         }
     }
 }
